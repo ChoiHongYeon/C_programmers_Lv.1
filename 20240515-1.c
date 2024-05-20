d08f6a4b2b4b0e96d53245780198006cca0ff41f
ChoiHongYeon <164014605+ChoiHongYeon@users.noreply.github.com>
2024-05-20 14:03:45 +0900

    second commit

--- 20240515-1.c
+++ 20240515-1.c
@@ -1,42 +1,40 @@
-﻿// [PCCP 기출문제] 1번 / 붕대 감기 (https://school.programmers.co.kr/learn/courses/30/lessons/250137?language=c)
+﻿// [PCCP 기출문제] 1번 / 붕대 감기 (https://school.programmers.co.kr/learn/courses/30/lessons/250137)
 
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
 
-// bandage_len은 배열 bandage의 길이입니다.
-// attacks_rows는 2차원 배열 attacks의 행 길이, attacks_cols는 2차원 배열 attacks의 열 길이입니다.
 int solution(int bandage[], size_t bandage_len, int health, int** attacks, size_t attacks_rows, size_t attacks_cols) {
     
-    int now_health = health;
-    int time = 0;
-    int attack_time = 0;
-    int success_time = 0;
+    int max_health = health;
+    int success = 0;
+    int attack = 0;
+    int last_attack = attacks[attacks_rows - 1][0];
 
-    for (time = 1; time <= attacks[attacks_cols - 1][0]; time++) {
-        if (time == attacks[attack_time][0]) {
-            success_time = 0;
-            now_health -= attacks[attack_time][1];
-            attack_time++;
+    for (int i = 1; i <= last_attack;i++) {
+        if (i == attacks[attack][0]) {
+            health -= attacks[attack][1];
+            success = 0;
+            attack++;
+            if (health <= 0)
+                break;
         }
         else {
-            now_health += bandage[1];
-            success_time += 1;
-            if (success_time == bandage[0]) {
-                success_time = 0;
-                now_health += bandage[2];
+            health += bandage[1];
+            success++;
+            if (success == bandage[0]) {
+                health += bandage[2];
+                success = 0;
             }
-            if (now_health > health)
-                now_health = health;
+            if (health > max_health) {
+                health = max_health;
+            }
         }
-        if (now_health <= 0)
-            break;
     }
     
-    int answer = now_health;
-    if (now_health <= 0)
+    if (health <= 0)
         return -1;
-    return answer;
+    return health;
 }
 
 void main()
@@ -53,7 +51,7 @@ void main()
     size_t size2 = 2;
     size_t size3 = 3;
     size_t size4 = 4;
-    
+
     int** attacks1 = (int**)malloc(size4 * sizeof(int*));
     attacks1[0] = (int*)malloc(2 * sizeof(int));
     attacks1[1] = (int*)malloc(2 * sizeof(int));
@@ -89,11 +87,10 @@ void main()
     int result1 = solution(bandage1, bandage_len, health1, attacks1, size4, size2);
     int result2 = solution(bandage2, bandage_len, health2, attacks2, size3, size2);
     int result3 = solution(bandage3, bandage_len, health3, attacks3, size3, size2);
-    int result4 = solution(bandage4, bandage_len, health4, attacks4, size4, size2);
+    int result4 = solution(bandage4, bandage_len, health4, attacks4, size2, size2);
 
     printf("%d\n", result1);
     printf("%d\n", result2);
     printf("%d\n", result3);
     printf("%d\n", result4);
-
 }
