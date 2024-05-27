c5d2cda028a238b4a448e52714deff1d4a0ab2b3
ChoiHongYeon <164014605+ChoiHongYeon@users.noreply.github.com>
2024-05-27 23:50:25 +0900

    second commit

--- 20240527-1.c
+++ 20240527-1.c
@@ -10,31 +10,33 @@
 // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 int* solution(const char* id_list[], size_t id_list_len, const char* report[], size_t report_len, int k) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
-    
-    int** relation = (int**)malloc(sizeof(int*) * id_list_len);
-    int* report_number = (int*)malloc(sizeof(int) * id_list_len);
-    char* report_a = (char*)malloc(sizeof(char) * report_len);
-    char* report_b = (char*)malloc(sizeof(char) * report_len);
 
-    for (int i = 0; i < id_list_len;i++) {
+    int** relation = (int**)malloc(sizeof(int*) * id_list_len);
+    int* reported_num = (int*)malloc(sizeof(int) * id_list_len);
+    int* answer = (int*)malloc(sizeof(int) * id_list_len);
+
+    for (int i = 0;i < id_list_len;i++) {
         relation[i] = (int*)malloc(sizeof(int) * id_list_len);
         for (int j = 0;j < id_list_len;j++) {
             relation[i][j] = 0;
         }
-        report_number[i] = 0;
+        reported_num[i] = 0;
+        answer[i] = 0;
     }
 
     for (int i = 0;i < report_len;i++) {
+        char A[11];
+        char B[11];
         int a, b;
-        sscanf(report[i], "%s %s", report_a[i], report_b[i]);
+        sscanf(report[i], "%s %s", A, B);
         for (int j = 0;j < id_list_len;j++) {
-            if (strcmp(report_a[i], id_list[j]) == 0) {
+            if (strcmp(A, id_list[j]) == 0) {
                 a = j;
                 break;
             }
         }
         for (int j = 0;j < id_list_len;j++) {
-            if (strcmp(report_b[i], id_list[j]) == 0) {
+            if (strcmp(B, id_list[j]) == 0) {
                 b = j;
                 break;
             }
@@ -43,31 +45,27 @@ int* solution(const char* id_list[], size_t id_list_len, const char* report[], s
             relation[a][b]++;
     }
 
-    int* answer = (int*)malloc(sizeof(int) * id_list_len);
-
     for (int i = 0;i < id_list_len;i++) {
         for (int j = 0;j < id_list_len;j++) {
-            report_number[i] += relation[i][j];
+            reported_num[i] += relation[j][i];
         }
-        answer[i] = 0;
     }
 
     for (int i = 0;i < id_list_len;i++) {
-        if (report_number[i] >= k) {
+        if (reported_num[i] >= k) {
             for (int j = 0;j < id_list_len;j++) {
                 if (relation[j][i] != 0)
                     answer[j]++;
             }
         }
     }
-
+    
     for (int i = 0;i < id_list_len;i++) {
         free(relation[i]);
     }
-    free(report_number);
-    free(report_a);
-    free(report_b);
-    
+    free(relation);
+    free(reported_num);
+
     return answer;
 }
 
@@ -89,17 +87,13 @@ void main()
     for (int i = 0;i < 4;i++) {
         printf("%d ", result1[i]);
     }
-    free(id_list1);
-    free(report1);
-    free(result1);
     printf("\n");
-
     for (int i = 0;i < 2;i++) {
         printf("%d ", result2[i]);
     }
-    free(id_list2);
-    free(report2);
-    free(result2);
     printf("\n");
+
+    free(result1);
+    free(result2);
 
 }
