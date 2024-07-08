214a74c19619a50222957b298e3b5a62e99a7cc0
ChoiHongYeon <164014605+ChoiHongYeon@users.noreply.github.com>
2024-07-08 22:54:20 +0900

    first commit

---
+++ 20240708-1.c
@@ -0,0 +1,19 @@
+﻿// 직사각형 별찍기 (https://school.programmers.co.kr/learn/courses/30/lessons/12969?language=c)
+
+#include <stdio.h>
+
+int main(void) {
+
+    int a;
+    int b;
+    scanf("%d %d", &a, &b);
+
+    for (int i = 0;i < b;i++) {
+        for (int j = 0;j < a;j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
