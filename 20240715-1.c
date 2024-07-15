5b9f20f36d0bf8eb1b197bbfa26b9fec5e0fc01c
ChoiHongYeon <164014605+ChoiHongYeon@users.noreply.github.com>
2024-07-15 17:05:26 +0900

    first commit

---
+++ 20240715-1.c
@@ -0,0 +1,16 @@
+﻿// 짝수와 홀수 (https://school.programmers.co.kr/learn/courses/30/lessons/12937?language=c)
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+char* solution(int num) {
+
+    return num % 2 ? "Odd" : "Even";
+
+}
+
+void main()
+{
+    printf("%s %s", solution(3), solution(4));
+}
