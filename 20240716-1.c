b36f546790fc5ebf2a5903e954bf3a6b47b371b7
ChoiHongYeon <164014605+ChoiHongYeon@users.noreply.github.com>
2024-07-16 20:52:24 +0900

    first commit

---
+++ 20240716-1.c
@@ -0,0 +1,24 @@
+﻿// 정수 제곱근 판별 (https://school.programmers.co.kr/learn/courses/30/lessons/12934?language=c)
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <math.h>
+
+long long solution(long long n) {
+
+    double s = sqrt(n);
+
+    if (fmod(s, 1.0) > 0.0)
+        return -1;
+
+    return (long long)(s + 1) * (long long)(s + 1);
+
+}
+
+void main()
+{
+
+    printf("%d %d", solution(121), solution(3));
+
+}
