7401d6464238c6b052a31900ec84ab80e85c4df2
ChoiHongYeon <164014605+ChoiHongYeon@users.noreply.github.com>
2024-09-03 20:43:39 +0900

    first commit

---
+++ 20240903-1.c
@@ -0,0 +1,40 @@
+﻿// 이상한 문자 만들기 (https://school.programmers.co.kr/learn/courses/30/lessons/12930?language=c)
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+char* solution(const char* s) {
+
+    char* answer = (char*)malloc(sizeof(char) * (strlen(s) + 1));
+    int count = 0;
+
+    for (int i = 0; i < strlen(s); i++) {
+        if (s[i] == ' ') {
+            answer[i] = ' ';
+            count = 0;
+        }
+        else if (count % 2 == 0) {
+            answer[i] = toupper(s[i]);
+            count++;
+        }
+        else {
+            answer[i] = tolower(s[i]);
+            count++;
+        }
+    }
+    answer[strlen(s)] = '\0';
+
+    return answer;
+}
+
+void main()
+{
+
+    const char* s = "try hello world";
+    char* result = solution(s);
+    printf("%s", result);
+
+}
